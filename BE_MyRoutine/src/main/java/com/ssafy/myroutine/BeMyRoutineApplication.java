52cb8c089f55519d3353056179d2b3354d244ab2
우혜지 <woohyeji@uhyejiui-MacBookPro.local>
2024-05-15 21:07:59 +0900

    feat: Spring Boot 프로젝트 생성

---
+++ BE_MyRoutine/src/main/java/com/ssafy/myroutine/BeMyRoutineApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.myroutine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMyRoutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMyRoutineApplication.class, args);
+	}
+
+}
---
+++ BE_MyRoutine/src/test/java/com/ssafy/myroutine/BeMyRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.myroutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMyRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
