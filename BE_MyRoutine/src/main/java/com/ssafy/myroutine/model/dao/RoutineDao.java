391b2b9a0fa12a4afc004d6927ca858bf0c7f790
wooqqq <sge0225@naver.com>
2024-05-16 23:28:12 +0900

    feat: 루틴 dao 및 mapper 구현

--- BE_MyRoutine/src/main/java/com/ssafy/myroutine/model/dao/RoutineDao.java
+++ BE_MyRoutine/src/main/java/com/ssafy/myroutine/model/dao/RoutineDao.java
@@ -1,13 +1,22 @@
 package com.ssafy.myroutine.model.dao;
 
+import java.util.List;
+
+import com.ssafy.myroutine.model.dto.Routine;
+import com.ssafy.myroutine.model.dto.SearchCondition;
+
 public interface RoutineDao {
-	
+
 	// 루틴 등록
-	
+	Routine insert(Routine routine);
+
 	// 루틴 수정
-	
+	int update(Routine routine);
+
 	// 루틴 삭제
-	
+	int delete(int id);
+
 	// 루틴 조회
-	
+	List<Routine> select(SearchCondition con);
+
 }
