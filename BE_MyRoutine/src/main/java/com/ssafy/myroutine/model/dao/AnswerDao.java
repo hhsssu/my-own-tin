a9e239fe76ff030cff3e2e36aefbc399ac839049
wooqqq <sge0225@naver.com>
2024-05-16 23:45:27 +0900

    feat: 답변 dao 및 mapper 구현

--- BE_MyRoutine/src/main/java/com/ssafy/myroutine/model/dao/AnswerDao.java
+++ BE_MyRoutine/src/main/java/com/ssafy/myroutine/model/dao/AnswerDao.java
@@ -1,11 +1,16 @@
 package com.ssafy.myroutine.model.dao;
 
+import com.ssafy.myroutine.model.dto.Answer;
+
 public interface AnswerDao {
 	
 	// 답변 등록
+	Answer insert(Answer answer);
 	
 	// 답변 수정
+	int update(Answer answer);
 	
 	// 답변 삭제
+	int delete(int id);
 	
 }
