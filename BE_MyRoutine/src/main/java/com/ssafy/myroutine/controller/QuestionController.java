325880456765519df0156d37a72568455bf7e991
wooqqq <sge0225@naver.com>
2024-05-17 10:33:21 +0900

    feat: controller 기본 세팅

--- BE_MyRoutine/src/main/java/com/ssafy/myroutine/controller/QuestionController.java
+++ BE_MyRoutine/src/main/java/com/ssafy/myroutine/controller/QuestionController.java
@@ -1,7 +1,29 @@
 package com.ssafy.myroutine.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.myroutine.model.service.AnswerService;
+import com.ssafy.myroutine.model.service.QuestionService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/myroutine/qna")
+@Tag(name = "QuestionController", description = "질문 및 답변 CRUD")
+// @CrossOrigin 추가 예정
 public class QuestionController {
 
+	private final QuestionService queService;
+	private final AnswerService ansService;
+	
+	@Autowired
+	public QuestionController(QuestionService queService, AnswerService ansService) {
+		this.queService = queService;
+		this.ansService = ansService;
+	}
+	
 	// 질문 등록
 
 	// 질문 수정
