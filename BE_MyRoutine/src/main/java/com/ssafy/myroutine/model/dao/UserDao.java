acc7408c46fee10ebc5c3d15658aef0db7a6da5b
wooqqq <sge0225@naver.com>
2024-05-17 10:49:07 +0900

    feat: 상세 조회 기능 관련 mapper 추가

--- BE_MyRoutine/src/main/java/com/ssafy/myroutine/model/dao/UserDao.java
+++ BE_MyRoutine/src/main/java/com/ssafy/myroutine/model/dao/UserDao.java
@@ -7,20 +7,15 @@ import com.ssafy.myroutine.model.dto.User;
 public interface UserDao {
 	
 	// 사용자 회원가입
-	User insertUser(User user);
+	User insert(User user);
 	
 	// 사용자 로그인 = 사용자 정보 조회
-	User loginUser(Map<String, String> user);
+	User selectOne(Map<String, String> user);
 	
 	// 사용자 정보 수정
-	int updateUser(User user);
+	int update(User user);
 	
 	// 사용자 회원탈퇴
-	int deleteUser(int id);
-	
-	// 사용자 정보 조회
-	// 로그인과 기능 통일할지 논의
-//	User selectOne(Map<String, String> info);
-	
+	int delete(int id);
 	
 }
