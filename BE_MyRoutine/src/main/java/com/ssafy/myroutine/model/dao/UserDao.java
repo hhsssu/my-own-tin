4fb589fb7ce9c085d2c0dad40265df6f326e726c
hhsssu <hhssuu14@naver.com>
2024-05-17 10:36:05 +0900

    feat: Dao 수정

--- BE_MyRoutine/src/main/java/com/ssafy/myroutine/model/dao/UserDao.java
+++ BE_MyRoutine/src/main/java/com/ssafy/myroutine/model/dao/UserDao.java
@@ -7,16 +7,16 @@ import com.ssafy.myroutine.model.dto.User;
 public interface UserDao {
 	
 	// 사용자 회원가입
-	User insert(User user);
+	User insertUser(User user);
 	
-	// 사용자 로그인
-	User login(Map<String, String> user);
+	// 사용자 로그인 = 사용자 정보 조회
+	User loginUser(Map<String, String> user);
 	
 	// 사용자 정보 수정
-	int update(User user);
+	int updateUser(User user);
 	
 	// 사용자 회원탈퇴
-	int delete(String id);
+	int deleteUser(int id);
 	
 	// 사용자 정보 조회
 	// 로그인과 기능 통일할지 논의
