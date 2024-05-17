42856e63ce6da87a23299b48922a6c5afa50d57a
hhsssu <hhssuu14@naver.com>
2024-05-17 15:11:02 +0900

    feat: QnA Controller 작성

--- BE_MyRoutine/src/main/java/com/ssafy/myroutine/controller/AnswerController.java
+++ BE_MyRoutine/src/main/java/com/ssafy/myroutine/controller/AnswerController.java
@@ -9,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,79 +17,66 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.myroutine.model.dto.Answer;
-import com.ssafy.myroutine.model.dto.Question;
 import com.ssafy.myroutine.model.dto.SearchCondition;
 import com.ssafy.myroutine.model.service.AnswerService;
-import com.ssafy.myroutine.model.service.QuestionService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/myroutine/que")
+@RequestMapping("/myroutine/ans")
 @Tag(name = "QuestionController", description = "질문 및 답변 CRUD")
 @CrossOrigin("http://localhost:5173")
-public class QuestionController {
+public class AnswerController {
 	// 응답을 편하게 하기 위해 상수로 지정
 	private static final String SUCCESS = "success";
 	private static final String FAIL = "fail";
+	
 
-	private final QuestionService queService;
+	private final AnswerService ansService;
 
 	@Autowired
-	public QuestionController(QuestionService queService) {
-		this.queService = queService;
+	public AnswerController(AnswerService ansService) {
+		this.ansService = ansService;
 	}
 
-	// 질문 등록
+	// 답변 등록
 	@Transactional
 	@PostMapping("/")
-	@Operation(summary = "질문 등록", description = "질문 등록 기능")
-	public ResponseEntity<?> write(@RequestBody Question que) {
-		queService.registQuestion(que);
-		return new ResponseEntity<Question>(que, HttpStatus.CREATED);
+	@Operation(summary = "답변 등록", description = "답변 등록 기능")
+	public ResponseEntity<?> write(@RequestBody Answer ans) {
+		ansService.registAnswer(ans);
+		return new ResponseEntity<Answer>(ans, HttpStatus.CREATED);
 	}
 
-	// 질문 수정
+	// 답변 수정
 	@Transactional
-//	@PutMapping("/{id}") --> PathVariable 처리와 ID 추가 작업 필요
 	@PutMapping("/")
-	@Operation(summary = "질문 수정", description = "질문 수정 기능")
-	public ResponseEntity<?> update(@RequestBody Question que) {
-		if(queService.modifyQuestion(que)) // true
+	@Operation(summary = "답변 수정", description = "답변 수정 기능")
+	public ResponseEntity<?> update(@RequestBody Answer ans) {
+		if(ansService.modifyAnswer(ans))
 			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
 		return new ResponseEntity<String>(FAIL, HttpStatus.BAD_REQUEST);
+		
 	}
 
-	// 질문 삭제
+	// 답변 삭제
 	@Transactional
 	@PutMapping("/delete")
-	@Operation(summary = "질문 삭제", description = "질문 삭제 기능")
+	@Operation(summary = "답변 삭제", description = "답변 삭제 기능")
 	public ResponseEntity<?> delete(@RequestParam int id) {
-		if(queService.removeQuestion(id)) // true
+		if(ansService.removeAnswer(id))
 			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
 		return new ResponseEntity<String>(FAIL, HttpStatus.BAD_REQUEST);
 	}
 	
-	// 질문 검색 조회
+	// 답변 전체 조회
 	@GetMapping("/")
-	@Operation(summary = "질문 검색", description = "질문 검색 기능")
 	public ResponseEntity<?> list(@ModelAttribute SearchCondition con) {
-		List<Question> list = queService.searchQuestion(con);
+		List<Answer> list = ansService.selectAllAnswer();
 		if(list == null || list.size() == 0)
 			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-		return new ResponseEntity<List<Question>>(list, HttpStatus.OK);
+		return new ResponseEntity<List<Answer>>(list, HttpStatus.OK);
 	}
-	
-	// 질문 상세 조회
-	@GetMapping("/{queId}")
-	@Operation(summary = "질문 상세 조회", description = "질문 상세 조회 기능")
-	public ResponseEntity<?> detail(@PathVariable("queId") int id) {
-		Question question = queService.getQuestion(id);
-		if(question != null)
-			return new ResponseEntity<Question>(question, HttpStatus.OK);
-		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
-	}
-
 
 }
