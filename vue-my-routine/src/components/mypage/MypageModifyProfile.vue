46f249942f269db7fc936950cae1d46536802b54
hhsssu <87113802+hhsssu@users.noreply.github.com>
2024-05-24 04:09:54 +0900

    feat: 키워드 추가 기능 완료

--- vue-my-routine/src/components/mypage/MypageModifyProfile.vue
+++ vue-my-routine/src/components/mypage/MypageModifyProfile.vue
@@ -1,52 +1,62 @@
 <template>
-  <div>
-    <h2>프로필 수정</h2>
-  </div>
+  <div class="modify-container">
+    <h2 class="view-title">프로필 수정</h2>
 
-  <!-- 프로필 이미지 선택 -->
-  <div>
-    <img src="@/assets/img/profile_female.png" alt="프로필이미지1" class="img-profile" />
-    <img src="@/assets/img/profile_male.png" alt="프로필이미지2" class="img-profile" />
-    <img
+    <!-- 프로필 이미지 선택 -->
+    <!-- <div>
+      <img src="@/assets/img/profile_female.png" alt="프로필이미지1" class="img-profile" />
+      <img src="@/assets/img/profile_male.png" alt="프로필이미지2" class="img-profile" />
+      <img
       src="@/assets/img/profile_gentlelady.png"
       alt="프로필이미지3"
       class="img-profile"
-    />
-    <img
+      />
+      <img
       src="@/assets/img/profile_gentleman.png"
       alt="프로필이미지4"
       class="img-profile"
-    />
+      />
+    </div> -->
+
+    <!-- 닉네임 수정 -->
+    <!-- <div>
+      <input type="text" v-model="nickname" />
+    </div> -->
+
+    <!-- 나의 성격 태그 수정 -->
+    <div class="profile-title">나의 성격 태그</div>
+    <div class="flex-box">
+      <label for="character1">키워드 1</label>
+      <input type="text" name="character1" id="character1" v-model="character1" />
+      <label for="character2">키워드 2</label>
+      <input type="text" name="character2" id="character2" v-model="character2" />
+      <label for="character3">키워드 3</label>
+      <input type="text" name="character3" id="character3" v-model="character3" />
+    </div>
+
+    <!-- 성격 태그 추천받기 - ChatGPT -->
+    <button @click="getCharacterTags" class="profile-btn" style="font-size: 1rem; background-color: #FFA101; margin: 20px 0 10px; padding: 5px;">성격 태그 추천받기</button>
+    <div v-html="promptmsg" class="ai-container"></div>
+
+    <button @click="modifyExTag" class="profile-btn">수정 완료</button>
   </div>
-
-  <!-- 닉네임 수정 -->
-  <div>
-    <input type="text" v-model="nickname" />
-  </div>
-
-  <!-- 나의 성격 태그 수정 -->
-  <div>
-    <input type="text" v-model="character1" />
-    <input type="text" v-model="character2" />
-    <input type="text" v-model="character3" />
-  </div>
-
-  <!-- 성격 태그 추천받기 - ChatGPT -->
-  <button @click="getCharacterTags">성격 태그 추천받기</button>
-  <div v-html="promptmsg"></div>
-
-  <button @click="modifyExTag">수정 완료</button>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { useUserStore } from "@/stores/user";
+
 import { OpenApiUtil } from "/src/assets/js/OpenApiUtil.js"; // 경로를 실제 파일 위치로 수정하세요.
 
+const userStore = useUserStore();
+
+const user = JSON.parse(sessionStorage.getItem("user"));
+
 const promptmsg = ref("");
-const nickname = ref("");
-const character1 = ref("");
-const character2 = ref("");
-const character3 = ref("");
+// const nickname = ref("");
+const character1 = ref(user.character1);
+const character2 = ref(user.character2);
+const character3 = ref(user.character3);
 
 // 성격 태그 추천 함수
 const getCharacterTags = async () => {
@@ -62,13 +72,67 @@ const getCharacterTags = async () => {
 
 // 수정 완료 함수 (필요에 따라 구현)
 const modifyExTag = () => {
-  // 수정 완료 로직 추가예정
   console.log("수정 완료 버튼 클릭됨");
+  const updatedUserProfile = {
+    id: user.id,
+    character1: character1.value,
+    character2: character2.value,
+    character3: character3.value,
+  };
+
+  userStore.updateLoginUser(updatedUserProfile);
 };
 </script>
 
 <style scoped>
 .img-profile {
   width: 100px;
+}
+
+.profile-title {
+  font-weight: 700;
+  margin-bottom: 15px;
+  margin-right: 15px;
+}
+
+.modify-container {
+  padding: 0 20px;
+}
+
+.flex-box {
+  flex-direction: column;
+  align-items: start;
+}
+
+.flex-box label {
+  margin: 10px 0 8px;
+}
+
+.flex-box input {
+  padding: 10px 5px;
+  border: none;
+  border-bottom: 1px solid black;
+}
+
+.profile-btn {
+  display: block;
+  margin: 50px auto;
+  text-align: center;
+  border: none;
+  background-color: #31525B;
+  color: #fff;
+  padding: 10px;
+  border-radius: 5px;
+  font-weight: 600;
+  font-size: 1.2rem;
+}
+
+.ai-container {
+  border: 1px solid #777; 
+  color: #555; 
+  padding: 15px; 
+  line-height: 1.5rem; 
+  border-radius: 5px; 
+  margin: 0 auto;
 }
 </style>
