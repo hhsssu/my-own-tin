d62b48b2c39c2ff1466341321c30035f941fc3b3
wooqqq <sge0225@naver.com>
2024-05-23 11:59:22 +0900

    remove: 필요없는 코드 및 주석 제거

--- BE_MyRoutine/src/main/java/com/ssafy/myroutine/controller/AnswerController.java
+++ BE_MyRoutine/src/main/java/com/ssafy/myroutine/controller/AnswerController.java
@@ -54,9 +54,6 @@ public class AnswerController {
 	@PutMapping("/")
 	@Operation(summary = "답변 수정", description = "답변 수정 기능")
 	public ResponseEntity<?> update(@RequestParam int id, @RequestBody Answer ans) {
-		
-		System.out.println(ans.getIsPicked());
-		
 		ans.setId(id);
 		if(ansService.modifyAnswer(ans))
 			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
